697f0f1d0725e8da6252572d29b030acc7a88a94
Siang Poh Chua <siang.poh.chua@autodesk.com>
2025-08-14 15:40:46 +0800

    change to client component

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,32 +1,19 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+"use client"
+import { signIn } from "next-auth/react";
 import { GithubLogoIcon } from "@phosphor-icons/react";
 
 export default function Page() {
-  const { status, data: session } = useSession();
   return (
     <div>
-      {!session && (
-        <button
-          type="button"
-          onClick={() => {
-            signIn("github");
-          }}
-        >
-          <GithubLogoIcon />
-          <span>Sign In with Github</span>
-        </button>
-      )}
-      {!!session && (
-        <button
-          type="button"
-          onClick={() => {
-            signOut();
-          }}
-        >
-          <GithubLogoIcon />
-          <span>Sign Out</span>
-        </button>
-      )}
+      <button
+        type="button"
+        onClick={() => {
+          signIn("github");
+        }}
+      >
+        <GithubLogoIcon />
+        <span>Sign In with Github</span>
+      </button>
     </div>
   );
 }
