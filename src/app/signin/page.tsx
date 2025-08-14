bd654fe494dc625189889b8d3577e09a2fe202a1
Chua Siang Poh <siangpoh@outlook.com>
2025-08-14 14:38:43 +0000

    add get session

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,12 @@
+import { getServerSession } from 'next-auth'
 import { SignInWithGithub } from "./_components/sign-in-with-github";
+import { Logger } from "./_components/logger";
 
-export default function Page() {
+export default async function Page() {
+  const session = await getServerSession()
   return (
     <div>
+      <Logger />
       <SignInWithGithub />
     </div>
   );
