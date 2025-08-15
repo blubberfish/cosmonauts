8f615e6f1d6e612cac734ffa82651edcb52bbe2d
Chua Siang Poh <siangpoh@outlook.com>
2025-08-15 22:55:47 +0000

    update dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { CONFIG } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { RightPocket } from "./_components/right-pocket";
+import { Logger } from "./_components/logger";
 
 export default async function Page() {
   const session = await getServerSession(CONFIG);
@@ -13,6 +14,7 @@ export default async function Page() {
       <header className="flex flex-row flex-nowrap items-center">
         <div className="flex-1"></div>
         <RightPocket profileImg={session.user?.image} />
+        <Logger />
       </header>
     </div>
   );
