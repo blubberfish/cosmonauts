87da82d25ab9161beadca23cc603bdda8e657e2d
Chua Siang Poh <siangpoh@outlook.com>
2025-08-24 15:45:59 +0000

    update home page

--- src/app/(static-site)/home/page.tsx
+++ src/app/(static-site)/home/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   return (
     <>
       <div className="snap-start relative h-screen w-full max-w-5xl mx-auto pt-32">
-        <div className="h-full w-full rounded-t-xl bg-black/67 text-white/80 p-8">
+        <div className="h-full w-full rounded-t-xl from-sky-400/67 to-violet-400/34 bg-linear-to-br border border-t-sky-300 border-l-sky-400 border-r-violet-400 border-b-0 text-white/80 p-8">
           <div className="flex flex-col items-center">
             <Computer />
             <div className="flex flex-col items-center mt-8">
@@ -20,7 +20,7 @@ export default async function Page() {
                 &nbsp;
               </p>
               <Link
-                className="px-6 py-2 mt-8 from-sky-400 to-violet-400 bg-linear-to-br hover:bg-linear-to-tl text-black/80 rounded-full"
+                className="px-6 py-2 mt-8 from-sky-400 to-violet-400 bg-linear-to-br hover:bg-linear-to-tl text-black/80 font-bold rounded-full"
                 href="/signin"
               >
                 Sign in to the dashboard
@@ -29,8 +29,8 @@ export default async function Page() {
           </div>
         </div>
       </div>
-      <div className="snap-start sticky top-0 h-screen w-full max-w-5xl mx-auto text-white bg-neutral-800"></div>
-      <div className="snap-start sticky top-0 h-screen w-full max-w-5xl mx-auto text-white bg-neutral-700"></div>
+      <div className="snap-start h-screen w-full max-w-5xl mx-auto text-white/80 from-sky-400/67 to-violet-400/34 bg-linear-to-tr border-l border-l-sky-400 border-r border-r-violet-400"></div>
+      <div className="snap-start h-screen w-full max-w-5xl mx-auto text-white/80 from-sky-400/67 to-violet-400/34 bg-linear-to-br border-l border-l-sky-400 border-r border-r-violet-400"></div>
     </>
   );
 }
