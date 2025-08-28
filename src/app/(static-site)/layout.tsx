63b0286d414bfebee1ed06b56edd90e48ec4fb87
Chua Siang Poh <siangpoh@outlook.com>
2025-08-28 14:23:30 +0000

    fix issues

--- src/app/(static-site)/layout.tsx
+++ src/app/(static-site)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Layout({ children }: Record<string, React.ReactNode>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="snap-mandatory snap-y h-screen w-screen bg-black overflow-auto">
       <div className="fixed top-0 left-0 stars"></div>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { SideBar, SideBarMenuButton } from "./_components";
 import { Session } from "@/lib/components";
 
-export default function Layout({ children }: Record<string, React.ReactNode>) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <header className="h-16 sticky top-0 left-0 right-0 px-8 flex flex-row items-center-safe bg-black text-white">
